cc2a5fd6f1d15d27ff0f01d324d69cf8e013a461
Mutualis <info@mutualis.com>
2026-06-17 00:34:36 -0400

    feat: proxy /api to backend in Vite dev server

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -4,6 +4,11 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   resolve: process.env.VITEST ? { conditions: ["browser"] } : undefined,
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["test/setup.ts"],
