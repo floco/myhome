926e73f8cd7a9e918e455e0d859ad8fd1c16fb50
Mutualis <info@mutualis.com>
2026-07-22 07:56:00 +0200

    feat(i18n): bootstrap svelte-i18n and add catalog completeness check

--- packages/editor/test/setup.ts
+++ packages/editor/test/setup.ts
@@ -5,3 +5,12 @@
   unobserve() {}
   disconnect() {}
 };
+
+import { register, init, waitLocale } from "svelte-i18n";
+import en from "../src/lib/locales/en.json";
+import fr from "../src/lib/locales/fr.json";
+
+register("en", () => Promise.resolve({ default: en }));
+register("fr", () => Promise.resolve({ default: fr }));
+init({ fallbackLocale: "en", initialLocale: "en" });
+await waitLocale();
